29feddf3b3ba7f7f25a6c3014ea337e44d574d88
Babak RezaeeDaryakenari <24599548+babakrezaee@users.noreply.github.com>
2019-03-13 20:16:14 +0300

    Create KpointsMat_fun.R

---
+++ Functions/KpointsMat_fun.R
@@ -0,0 +1,22 @@
+
+# DF=DataFrame
+# Vec=Vector
+# K= number of points in the neighborhood
+
+EucDis_fun<-function(DF,vec){
+  
+  Euc_di=rep(0,nrow(DF))
+  
+  for (i in 1:length(X_f)){
+    Euc_di=Euc_di+(DF[,i]-vec[i])^2
+  }
+  return(Euc_di^.5)
+}
+
+
+Kpoints <- function(DF, vec, k){
+  
+  Euc_di=EucDis_fun(DF,vec)
+  
+  return(X[order(Euc_di),][1:k,])
+}
